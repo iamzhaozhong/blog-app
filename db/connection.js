470fce8f3c19662f2de3c866a37b053b8c0aaab4
Zhao Zhong <zhzhong@sas.upenn.edu>
2021-12-16 18:43:55 -0700

    heroku app test

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-let MONGODB_URI = process.env.PROD_MONGODB || 3000
+let MONGODB_URI = process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/blogAppDatabase";
 
 mongoose.set('returnOriginal', false);
 
