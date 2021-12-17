aa2aaf878040077ad905bdd46297a7e29a1267d6
Kaheem Freeman <kfreeman@Kaheems-MBP-2.lan>
2021-12-17 15:53:43 -0500

    finished adding routes

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,20 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
 let apiUrl;
 
 const apiUrls = {
-	production: 'TO BE ADDED',
-	development: 'http://localhost:3000/api'
-}
+  production: "https://velvet-blog-app.herokuapp.com/api",
+  development: "http://localhost:3000/api",
+};
 
-if (window.location.hostname === 'localhost') {
-	apiUrl = apiUrls.development
+if (window.location.hostname === "localhost") {
+  apiUrl = apiUrls.development;
 } else {
-	apiUrl = apiUrls.production
+  apiUrl = apiUrls.production;
 }
 
-const api = axios.create ({
-	baseURL: apiUrl
-})
+const api = axios.create({
+  baseURL: apiUrl,
+});
 
 export default api;
