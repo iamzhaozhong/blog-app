95f9c3806388fed90036b3da6c6a9ae54f178155
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-12-20 12:35:21 -0500

    trouble shooting our data render

--- client/src/screens/Posts.js
+++ client/src/screens/Posts.js
@@ -1,28 +1,31 @@
-import {useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 // import Posts from './Posts.css'
- import { getPosts } from "../services/posts";
+import { getPosts } from "../services/posts";
 import React from 'react'
-import api from "../services/apiConfig";
+// import api from "../services/apiConfig";
+
+
 function Posts() {
   const [blogPosts, setBlogPosts] = useState([])
   useEffect(() => {
     const fetchPosts = async () => {
       const allPosts = await getPosts()
       setBlogPosts(allPosts)
+      console.log(allPosts)
     }
-fetchPosts()
-  },[])
-   return (
-     <div className="posts">
-       {blogPosts.map((post, index) => {
-         <div >
-           <h1 className="post">{post.title}</h1>
-           <h2 className="post" >{post.author}</h2>
-           <h3 className="post">{post.topic}</h3>
-           <p className="post">{post.content}</p>
-     </div>
-    })}
+    fetchPosts()
+  }, [])
+  return (
+    <div className="posts">
+      {blogPosts.map((post, index) => {
+        <div >
+          <h1 className="post">{post.title}</h1>
+          <h2 className="post" >{post.author}</h2>
+          <h3 className="post">{post.topic}</h3>
+          <p className="post">{post.content}</p>
+        </div>
+      })}
     </div>
-   )
- }
- export default Posts
+  )
+}
+export default Posts
