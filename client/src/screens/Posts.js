8a6036ad60a8820eb013798586cac20b7d5b371c
Nicholas Campanella <nicholascampanella@Nicholass-MacBook-Pro.local>
2021-12-20 09:34:42 -0700

    update

--- client/src/screens/Posts.js
+++ client/src/screens/Posts.js
@@ -1,18 +1,28 @@
 import {useEffect, useState } from "react";
-// import Posts from './Posts.css' 
- 
-
+// import Posts from './Posts.css'
+ import { getPosts } from "../services/posts";
 import React from 'react'
- 
+import api from "../services/apiConfig";
 function Posts() {
   const [blogPosts, setBlogPosts] = useState([])
-  
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const allPosts = await getPosts()
+      setBlogPosts(allPosts)
+    }
+fetchPosts()
+  },[])
    return (
      <div className="posts">
-       
+       {blogPosts.map((post, index) => {
+         <div >
+           <h1 className="post">{post.title}</h1>
+           <h2 className="post" >{post.author}</h2>
+           <h3 className="post">{post.topic}</h3>
+           <p className="post">{post.content}</p>
      </div>
+    })}
+    </div>
    )
  }
- 
  export default Posts
- 
