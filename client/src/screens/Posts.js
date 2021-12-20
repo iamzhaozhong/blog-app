d2beb6c9c91c7f6287ec389db9be05a5fa956b2a
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-12-20 16:24:14 -0500

    posts now render to DOM, debugged .map

--- client/src/screens/Posts.js
+++ client/src/screens/Posts.js
@@ -6,25 +6,36 @@ import React from 'react'
 
 
 function Posts() {
-  const [blogPosts, setBlogPosts] = useState([])
+  // const [blogPosts, setBlogPosts] = useState([])
+  const [posts, setPosts] = useState([])
+
   useEffect(() => {
     const fetchPosts = async () => {
       const allPosts = await getPosts()
-      setBlogPosts(allPosts);
+      setPosts(allPosts)
+
     }
     fetchPosts()
   }, [])
+
+
   return (
+
     <div className="posts">
-      {blogPosts.map((post, index) => {
-        <div >
-          <h1 className="post">{post.title}</h1>
-          <h2 className="post" >{post.author}</h2>
-          <h3 className="post">{post.topic}</h3>
-          <p className="post">{post.content}</p>
-        </div>
+      {posts.map((post, index) => {
+        return (
+          <div key={index}>
+            <h1 className="post">{post.title}</h1>
+            <h2 className="post" >{post.author}</h2>
+            <h3 className="post">{post.topic}</h3>
+            <p className="post">{post.content}</p>
+          </div>
+        )
+        {/* console.log(post) */ }
       })}
+
     </div>
+
   )
 }
 export default Posts
